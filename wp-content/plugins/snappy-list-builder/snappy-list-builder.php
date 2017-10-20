03dda1c99739df1d1fe59a1b08a97dcc18058407
vicente noriega <vicente.noriega@thlink.marketing>
2017-10-20 13:21:15 -0500

    SE 3 - 12. Building a custom Shortcode.

--- wp-content/plugins/snappy-list-builder/snappy-list-builder.php
+++ wp-content/plugins/snappy-list-builder/snappy-list-builder.php
@@ -11,3 +11,108 @@ License: GPL2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: snappy-list-builder
 */
+
+/* !0. TABLE OF CONTENTS */
+
+/*
+	1. HOOKS
+	1.1 - hint: register all our custom shortcodes on init	
+
+	2. SHORTCODES
+		2.1 - slb_register_shortcodes()
+		2.2 - slb_form_shortcode()
+
+	3. FILTERS
+
+	4. EXTERNAL SCRIPTS
+
+	5. ACTIONS
+
+	6. HELPERS
+
+	7. CUSTOM POST TYPES
+
+	8. ADMIN PAGES
+
+	9. SETTINGS
+*/
+
+/* !1. HOOKS */
+
+// 1.1
+// hint: register all our custom shortcodes on init
+add_action( 'init', 'slb_register_shortcodes' );
+
+/* !2. SHORTCODES */
+
+// 2.1
+function slb_register_shortcodes() {
+
+	add_shortcode( 'slb_form', 'slb_form_shortcode' );
+}
+
+// 2.2
+function slb_form_shortcode( $args, $content="" ) {
+	// setup output variable - the form html
+	$output = '
+		<div class="slb">
+			
+			<form id="slb_form" name="slb_form" class="slb-form" method="post">
+
+				<p class="slb-input-container">
+
+					<label>Your Name</label><br />
+					<input type="text" name="slb_fname" placeholder="First Name" />
+					<input type="text" name="slb_lname" placeholder="Last Name" />
+
+				</p>
+
+				<p class="slb-input-container">
+
+					<label>Your Email</label><br />
+					<input type="email" name="slb_email" placeholder="ex. you@email.com" />
+
+				</p>';
+
+				// including content in our html if content is passed into the function
+				if( strlen( $content ) ):
+
+					$output .= '<div class="slb-content>'. wpautop( $content ) . '</div>';
+
+				endif;
+
+				// completing our form html
+				$output .= '<p class="slb-input-container">
+
+					<input type="submit" name="slb_submit" value="Sign Me Up!" />
+
+				</p>
+
+			</form>
+
+		</div>
+	';
+
+	// return our results/html
+	return $output;
+}
+
+/* !3. FILTERS */
+
+
+/* !4. EXTERNAL SCRIPTS */
+
+
+/* !5. ACTIONS */
+
+
+/* !6. HELPERS */
+
+
+/* !7. CUSTOM POST TYPES */
+
+
+/* !8. ADMIN PAGES */
+
+
+/* !9. SETTINGS */
